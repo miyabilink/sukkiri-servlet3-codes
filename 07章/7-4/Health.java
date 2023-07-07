1ae27d40a852c37a3647d1de9ddbe7c8c3ff4bd5
Takuma SUGAWARA <sugawara@miyabilink.jp>
2023-07-07 10:38:07 +0900

    コード7-1にthisを追加

--- 07章/7-4/Health.java
+++ 07章/7-4/Health.java
@@ -6,12 +6,12 @@ public class Health implements Serializable {
   private double height, weight, bmi;
   private String bodyType;
 
-  public double getHeight() { return height; }
-  public void setHeight(Double height) { this.height = height; }
-  public double getWeight() { return weight; }
-  public void setWeight(Double weight) { this.weight = weight; }
-  public void setBmi(Double bmi) { this.bmi = bmi; }
+  public double getHeight() { return this.height; }
+  public void setHeight(double height) { this.height = height; }
+  public double getWeight() { return this.weight; }
+  public void setWeight(double weight) { this.weight = weight; }
   public double getBmi() { return this.bmi;}
-  public void setBodyType(String bodyType) { this.bodyType = bodyType; }
+  public void setBmi(double bmi) { this.bmi = bmi; }
   public String getBodyType() { return this.bodyType; }
+  public void setBodyType(String bodyType) { this.bodyType = bodyType; }
 }
